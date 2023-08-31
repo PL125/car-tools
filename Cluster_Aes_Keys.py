6c1571414954035e0f2c4f0e6d47f8ccddde55ac
dnoermann <dennis.noermann@noernet.de>
2023-08-31 10:53:44 +0200

    Adding stuf for offline CP matching CP Removing and Swap Key Changing for PQ25/PQ35 Bosch MRRevo14 Radars

---
+++ Cluster_Aes_Keys.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# (C) Dennis Noermann 2023
+# This is free for all
+# You are free to support me via Paypal dennis.noermann@noernet.de
+#
+
+# All my AES Keys for Clusters .....
+
+
+# this is the last 16 Bytes of the 32 Byte AES Key from Cluster
+# it can be read out with genuine VAGTacho
+# Or any other good Cluster tool
+# The 32 Byte Key can be read from Cluster Flash @ Address 0x10100 
+
+keyhex = '000102030405060708090A0B0C0D0E0F'  # you need to change this
+
